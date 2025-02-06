86a4752b656b7a32eb51fa5e04abc329c9f07fda
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2025-02-06 19:44:20 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.{js,jsx}',  // ou o caminho correto dos seus arquivos
+    'src/**/*.{js,jsx}',
     '!src/**/*.test.{js,jsx}',  // exclui os arquivos de teste da coleta
-  ],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
-    },
-  },
+  ]
 };
